e8b122785fedbaa9f21e18fb45dff63890f9ab72
Evans <denziedon@gmail.com>
2024-02-23 03:47:16 +0300

    Created a failing Test

---
+++ Cashcard/src/test/java/com/example/Cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package com.example.Cashcard;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+
+    //annotation is part of junit library
+    @Test
+    void myFirstTest(){
+        //assertThat is part of assertj library
+        assertThat(42).isEqualTo(42);
+    }
+}
